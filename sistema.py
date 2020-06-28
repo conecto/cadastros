e555f49b0ddee4ed0636ed5f065d0dd6b856d872
Everton Dutra <evertondutra02@gmail.com>
2020-06-27 23:01:03 -0300

    sistema gravando no sqlite

--- sistema.py
+++ sistema.py
@@ -1,6 +1,11 @@
 
 from lib.arquivo import *
 
+connection = sqlite3.connect('cad.db')
+c = connection.cursor()
+
+create_table()
+
 arq = 'DadosUsuarios.txt'
 if not arquivoExiste(arq):
     criarArquivo(arq)
@@ -8,11 +13,26 @@ titulo('MENU PRINCIPAL')
 while True:
     resposta = menu(['Cadastrar Usuário', 'Consultar Pessoa Cadastrada', 'Sair'])
     if resposta == 1:
-        subtitulo('Cadastrar usuário')
-        nome = input('Nome: ')
-        user = usuarioConfirma('Usuário: ', arq)
-        senha = leiaSenha('Senha: ')
+        while True:
+            subtitulo('Cadastrar usuário')
+            nome = input('Nome: ')
+            user = usuarioConfirma('Usuário: ', arq)
+            senha = leiaSenha('Senha: ')
+            while True:
+                print(linha())
+                conf = input(f"""Confirme seus dados [S/N]:
+Nome = {nome}
+Usuário = {user}
+R: """).strip().upper()[0]
+                if conf not in "SN":
+                    print('\033[31mDigite uma opção válida!\033[m')
+                else:
+                    break
+            if conf == 'S':
+                break
+
         cadastrar(arq, nome, user, senha)
+        dataentry(nome, user, senha)
     elif resposta == 2:
         while True:
             print(linha())
